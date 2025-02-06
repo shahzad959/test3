e217f5c6d2deffc8877fde9f39e9321424c02219
shahzad959 <shahzad_chauhan@hotmail.com>
2025-02-06 22:42:15 +0500

    Commit

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -1,10 +1,12 @@
-let length;
-let width;
+let grocery1;
+let grocery2;
+let grocery3;
 
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
+function groceryTracker() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    let area = grocery1 + grocery2 + grocery3
 
-    let area = length * width;
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    document.getElementById('result').innerText = `Total Amount is $: ${area}`;
 }
